a3fa768032a5adc0ff0af8a3e38471206d3727ab
lilithsdemon <lucy.gardener2004@gmail.com>
2025-03-05 03:51:40 +0000

    Fix getting a consistent naming

--- src/main/java/net/lilithsdemon/beanzz/beanzz.java
+++ src/main/java/net/lilithsdemon/beanzz/beanzz.java
@@ -14,14 +14,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(net.lilithsdemon.beanzz.Beanzz.MODID)
-public class Beanzz
+@Mod(beanzz.MODID)
+public class beanzz
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "beanzz";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    public Beanzz()
+    public beanzz()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
